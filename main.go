0e4da9ef933dde7fd1a6812e15c819c7b5d2b503
sstanton <steven@thesimpledev.com>
2025-01-03 12:44:24 -0500

    Initial upload

--- main.go
+++ main.go
@@ -1,5 +1,44 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
 func main() {
+	reader := bufio.NewReader(os.Stdin)
+	printPrompt()
+	for {
+		input, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+		}
+
+		if err = processCommand(input); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+		}
+	}
+}
+
+func processCommand(command string) error {
+	cleanCommand := strings.TrimSuffix(command, "\n")
+
+	if cleanCommand == "" {
+		return nil
+	}
+
+	switch cleanCommand {
+	case "exit":
+		os.Exit(0)
+	default:
+	}
+	printPrompt()
+	return nil
+}
+
+func printPrompt() {
+	fmt.Print("> ")
 
 }
